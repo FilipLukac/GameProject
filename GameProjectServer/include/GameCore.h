dd7edc40e2c541eea068e40bb3e995e60c4ff173
Chipsi <filip.lukac@gmail.com>
2012-11-01 12:58:03 +0100

    Just image our server handler
    Signed-off-by: Chipsi <filip.lukac@gmail.com>

--- GameProjectServer/include/GameCore.h
+++ GameProjectServer/include/GameCore.h
@@ -4,6 +4,17 @@
 class GameProjectServer
 {
 public:
+
+    void HandleChangedHp(/*Packet *data*/);
+    void HandleChangedMana(/*Packet *data*/);
+    void HandleChangePosition(/*Packet *data*/);
+
+
+    void HandleLoginInWorld(/*Packet *data*/);
+    void HandleLogoutWorld/*Packet *data*/();
+    void HandleUsernameAndPassword(/*Packet *data*/);
+
+
     GameProjectServer();
     virtual ~GameProjectServer();
 }
