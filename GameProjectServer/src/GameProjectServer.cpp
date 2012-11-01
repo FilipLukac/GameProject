bc112dae0c23488daeadb0623a022d03a0aef1e2
Chipsi <filip.lukac@gmail.com>
2012-11-01 17:10:49 +0100

    daco zmenim
    Signed-off-by: Chipsi <filip.lukac@gmail.com>

--- GameProjectServer/src/GameProjectServer.cpp
+++ GameProjectServer/src/GameProjectServer.cpp
@@ -17,14 +17,12 @@ void GameProjectServer::LoadPlayerData()
     data.pos.y = 1.0f;
     data.pos.z = 1.0f;
 
-
     m_playerMap[1] = "Test";
     std::cout << "Player data loaded in " << getMSTimeDiff(oldTime, time(NULL)) << "seconds" << std::endl;
 }
 
 uint32 GameProjectServer::getMSTimeDiff(uint32 oldMSTime, uint32 newMSTime)
 {
-    // getMSTime() have limited data range and this is case when it overflow in this tick
     if (oldMSTime > newMSTime)
         return (0xFFFFFFFF - oldMSTime) + newMSTime;
     else
